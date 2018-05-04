3af8645abfa36d882a41a93213e33004082b6836
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2018-05-04 15:58:10 +0100

    Up to the end of chapter 4

---
+++ src/main/scala/chapter04/Either.scala
@@ -0,0 +1,45 @@
+package chapter04
+
+import chapter03.{Cons, List}
+
+import scala.util.{Failure, Success, Try}
+
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B]
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B]
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
+}
+
+case class Left[+E](value: E) extends Either[E, Nothing]{
+  override def map[B](f: Nothing => B): Either[E, B] = this
+  override def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Either[EE, B] = this
+  override def orElse[EE >: E, B >: Nothing](b: => Either[EE, B]): Either[EE, B] = b
+  override def map2[EE >: E, B, C](b: Either[EE, B])(f: (Nothing, B) => C): Either[EE, C] = this
+}
+
+
+case class Right[+A](value: A) extends Either[Nothing, A] {
+  override def map[B](f: A => B): Either[Nothing, B] = Right(f(value))
+  override def flatMap[EE >: Nothing, B](f: A => Either[EE, B]): Either[EE, B] = f(value)
+  override def orElse[EE >: Nothing, B >: A](b: => Either[EE, B]): Either[EE, B] = this
+  override def map2[EE >: Nothing, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = b.map(c => f(value,c))
+}
+
+object EitherFunctions {
+
+  def withTry[A](t: Try[A]):Either[String, A] = {
+    t match {
+      case _:Failure[A] => Left("Try has failed!")
+      case Success(x) => Right(x)
+    }
+  }
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = {
+    es.foldRight[Either[E, List[A]]](Right(List()))((a:Either[E,A], b:Either[E, List[A]]) => b.flatMap(c => a.map(sa => Cons(sa, c))))
+  }
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    as.foldRight[Either[E, List[B]]](Right(List()))((a:A, b:Either[E, List[B]]) => b.flatMap(c => f(a).map(sa => Cons(sa, c))))
+  }
+}
---
+++ src/test/scala/chapter04/EitherSpecs.scala
@@ -0,0 +1,77 @@
+package chapter04
+
+import chapter03.List
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.util.Try
+
+class EitherSpecs extends FlatSpec with Matchers {
+
+  def sqrt(n:Int) = if(n<0) Left("Complex number!") else Right(Math.sqrt(n))
+
+  "map" should "map over Right" in {
+    val right = Right(1)
+    right.map(_ + 1) shouldBe Right(2)
+  }
+
+  "map" should "map over Left" in {
+    val none = Left("Complex number!")
+    none.map((n:Int) => n + 1) shouldBe Left("Complex number!")
+  }
+
+  "flatMap" should "map over Right" in {
+    val right = Right(1)
+
+    right.flatMap(sqrt) shouldBe Right(1.0)
+  }
+
+  "flatMap" should "map over some for bad values" in {
+    val someBadValue = Right(-1)
+    someBadValue.flatMap(sqrt) shouldBe Left("Complex number!")
+  }
+
+  "flatMap" should "map over Left" in {
+    val left = Left("Some old problem")
+    left.flatMap(sqrt) shouldBe Left("Some old problem")
+  }
+
+  "map2" should "give the correct result for supplied params" in {
+    Right(4).map2(Right(2))((a:Int,b:Int) => a + b) shouldBe Right(6)
+  }
+
+  it should "give the correct result for Left first param" in {
+    Left("").map2(Right(2))((a:Int,b:Int) => a + b) shouldBe Left("")
+  }
+
+  it should "give the correct result for Left second param" in {
+    Right(2).map2(Left(""))((a:Int,b:Int) => a + b) shouldBe Left("")
+  }
+
+  it should "give the correct result for Left params" in {
+    Left("").map2(Left(""))((a:Int,b:Int) => a + b) shouldBe Left("")
+  }
+
+  "sequence" should "give the correct result for a list of all data" in {
+    val testData: List[Either[String, Int]] = List(Right(1), Right(2), Right(3))
+    val expectedResult: Either[String, List[Int]] = Right(List(1, 2, 3))
+    EitherFunctions.sequence(testData) shouldBe expectedResult
+  }
+
+  it should "give the correct result of None for list containing Left" in {
+    val testData: List[Either[String, Int]] = List(Right(1), Left(""), Right(3))
+    val expectedResult: Either[String, List[Int]] = Left("")
+    EitherFunctions.sequence(testData) shouldBe expectedResult
+  }
+
+  "traverse" should "give the correct result for a list of all correct" in {
+    val testData:List[String] = List("1", "2", "3")
+    val expectedResult: Either[String, List[Int]] = Right(List(1, 2, 3))
+    EitherFunctions.traverse(testData)((s:String) => EitherFunctions.withTry(Try(s.toInt))) shouldBe expectedResult
+  }
+
+  it should "give the correct result of None for list containing Left" in {
+    val testData:List[String] = List("1", "&", "3")
+    val expectedResult: Either[String, List[Int]] = Left("Try has failed!")
+    EitherFunctions.traverse(testData)((s:String) => EitherFunctions.withTry(Try(s.toInt))) shouldBe expectedResult
+  }
+}
