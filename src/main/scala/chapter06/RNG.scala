64f0ae403d9040568e480f22bc57c52ba071f3d5
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2019-10-11 15:34:15 +0100

    master: Ex 6.8

--- src/main/scala/chapter06/RNG.scala
+++ src/main/scala/chapter06/RNG.scala
@@ -17,6 +17,21 @@ object RNG {
       (f(a), rng2)
     }
 
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
+    rng => {
+      val (a, rng2) = f(rng)
+      g(a)(rng2)
+    }
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt){
+      i => val mod = i % n
+        if (i + (n-1) - mod >= 0)
+          unit(mod)
+        else nonNegativeLessThan(n)
+    }
+
   def nonNegativeEven: Rand[Int] =
     map(nonNegativeInt)(i => i - i % 2)
 
