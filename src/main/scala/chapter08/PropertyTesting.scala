5a16484e9c891eb4a71e02d20bbf6c80bf61eab7
daniel.manning <daniel.manning@digital.hmrc.gov.uk>
2020-03-13 15:13:26 +0000

    master: Implement generator functions

--- src/main/scala/chapter08/PropertyTesting.scala
+++ src/main/scala/chapter08/PropertyTesting.scala
@@ -28,20 +28,43 @@ trait Prop {
 
 
 case class Gen[A](sample: State[RNG,A]) {
-  def unit[A](a: => A): Gen[A] = ???
-  def boolean: Gen[Boolean] = ???
-  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = ???
+  def unit[A](a: => A): Gen[A] = Gen(sample.map(_ => a))
 }
 
 object Gen {
   def choose(start: Int, stopExclusive: Int): Gen[Int] =
     Gen(State(RNG.nonNegativeInt)
       .map(n => start + n % (stopExclusive - start)))
+
+  def boolean: Gen[Boolean] = Gen(State{rng =>
+    val (int, rngNext) = rng.nextInt
+    if(int < 0) (rngNext, false)
+    else (rngNext, true)
+  })
+
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen(
+    State(r =>  (1 to n).foldRight((r, List[A]())){
+        (_, b) => {
+          val (rngNext, a) = g.sample.run(b._1).value
+
+          (rngNext, a :: b._2)
+        }
+      }
+    ))
+
+  State
 }
 
 object ohNo extends App {
   val a = Gen.choose(0, 12)
     .sample.run(SimpleRNG(48l))
   println(a.value._2)
+
+  val b = Gen.boolean.sample.run(SimpleRNG(42l))
+  println(b.value._2)
+
+  val c = Gen.listOfN(7, Gen.choose(0, 12)).sample.run(SimpleRNG(0l))
+  println(c.value._2)
 }
 
